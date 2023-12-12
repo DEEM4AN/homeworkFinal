7631fb62d27759c1ee14f3345b4668b16ac73a18
Dima Bely <deem4an@gmail.com>
2023-12-12 19:35:15 +0300

    Task1 complete!

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Task1
+{
+    public static void Main(string[] args)
+    {
+        int M = 3;
+        int N = 14;
+        PrintNaturalNumbers(M, N);
+    }
+    static void PrintNaturalNumbers(int x, int y)
+    {
+        if (x < y)
+        {
+            Console.WriteLine(x);
+            x++;
+            PrintNaturalNumbers(x, y);
+        } 
+        else if (y < x) {
+            Console.WriteLine(y);
+            y++;
+            PrintNaturalNumbers(x, y);
+        } else Console.WriteLine(x);
+    }
+}
